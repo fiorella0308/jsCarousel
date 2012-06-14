28a7d88c81eedf8d5af4bda266d08555fe7c14d8
Juanma Santoyo <juanma@juanmasantyo.es>
2012-06-14 07:15:23 +0200

    JavaScript de ejemplo

--- js/sample.js
+++ js/sample.js
@@ -1,4 +1,40 @@
 ﻿$(document).ready(function ()
-{ 
+{
+    $('#carrusel').jsCarousel(
+    {
+        autostart: true
+        , effect: 'fade'
+        , width: 400
+        , height: 400
+    })
 
+    $('#start-button').click(function (e)
+    {
+        $('#carrusel').jsCarousel('start');
+    });
+
+    $('#stop-button').click(function (e)
+    {
+        $('#carrusel').jsCarousel('stop');
+    });
+
+    $('#apply').click(function (e)
+    {
+        $('#carrusel').jsCarousel('options'
+        , {
+            effect: $('#effect').eq(0).val()
+            , effectDuration: parseInt($('#effectDuration').eq(0).val())
+            , interval: parseInt($('#interval').eq(0).val())
+        });
+    });
+
+    $('#next').click(function (e)
+    {
+        $('#carrusel').jsCarousel('next');
+    });
+
+    $('#previous').click(function (e)
+    {
+        $('#carrusel').jsCarousel('previous');
+    });
 });
